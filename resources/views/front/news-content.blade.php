f69ad2fb75a853d2615ac933292aa7e7199aa2b3
Sophia Fang <sophiafang0617@gmail.com>
2022-01-14 15:04:06 +0800

    2022-01-14 (news)

--- resources/views/front/news-content.blade.php
+++ resources/views/front/news-content.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'TINGS News Content')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/news-content.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/news-content.css') }}">
 @endsection
 
 @section('main')
@@ -11,15 +11,15 @@
     <section id="news-header">
         <div class="nav"></div>
         <div class="container">
-            <h1>NEWS</h1>
+            <h1>News</h1>
             <div class="content">
                 <h3 class="description">
                     <span>以自製香氛，</span>
-                    <span>開啟2022年度。</span>
-                    <span>讓整年度，</span>
-                    <span>都充滿迷人香氣。</span>
-                    <span>TINGS Candle 香氛•蠟燭•台中手作教室。</span>
-                    <span class="scroll">SCROLL</span>
+                    <span>開啟 <span class="numbers">2022</span> 年度。</span>
+                    <span>讓整個年度，</span>
+                    <span>充滿迷人香氣。</span>
+                    <span><span class="english">TINGS Aroma</span> 香氛 • 蠟燭 • 台中手作教室。</span>
+                    <span class="scroll">Scroll</span>
                 </h3>
                 <div class="photo-full">
                     <div class="top"></div>
@@ -29,29 +29,30 @@
             <div class="background-logo"></div>
         </div>
     </section>
-    
+
     <!-- News content -->
     <section id="news-content">
         <div class="news">
-            <div class="title">2021年的最後一天</div>
+            <div class="title">2021 年的最後一天</div>
             <div class="date">2021.12.31</div>
             <div class="content">
-                <p>2021年的最後一天，還是跟學員們渡過，結束最後一堂課。</p>
-                <p>感謝今年所有參與TINGS Candle課程的同學們，祝大家新年快樂！</p>
+                <p>2021 年的最後一天，還是跟學員們渡過，結束最後一堂課。</p>
+                <p>感謝今年所有參與 TINGS Candle 課程的同學們，祝大家新年快樂！</p>
                 <p>新年假期一樣，跟大家一起過。</p>
-                <p>※市集活動：台北捷運中山站R9出口@心中山線型公園1/1（六），1/2（日）12:00-20:00</p>
-                <p>＊小容量香氛蠟燭新登場，歡迎直接現場聞香選購。</p>
-                <p>※台中體驗課程：香氛製品調香體驗1/2 （日）上午10:00</p>
-                <p>＊其他課程預約請私訊詢問。</p>
+                <p>* 市集活動：台北捷運中山站 R9 出口 @心中山線型公園 1/1 (六)，1/2 (日) 12:00 - 20:00</p>
+                <p>* 小容量香氛蠟燭新登場，歡迎直接現場聞香選購</p>
+                <p>* 台中體驗課程：香氛製品調香體驗 1/2 (日) 上午 10:00</p>
+                <p>* 其他課程預約請私訊詢問</p>
             </div>
-        <div class="button">
-            <a class="prev">Prev</a>
-            <a class="back-to-list">Back to list</a>
-            <a class="next">Next</a>
+            <div class="button">
+                <a class="prev">Previous</a>
+                <a href="{{ route('front.news-list') }}" class="back-to-list">Back to list</a>
+                <a class="next">Next</a>
+            </div>
         </div>
     </section>
 @endsection
 
 @section('js')
-    
+
 @endsection
--- resources/views/front/news-list.blade.php
+++ resources/views/front/news-list.blade.php
@@ -7,19 +7,19 @@
 @endsection
 
 @section('main')
-     <!-- News 首頁 -->
-     <section id="news-header">
+    <!-- News 首頁 -->
+    <section id="news-header">
         <div class="nav"></div>
         <div class="container">
-            <h1>NEWS</h1>
+            <h1>News</h1>
             <div class="content">
                 <h3 class="description">
                     <span>以自製香氛，</span>
-                    <span>開啟2022年度。</span>
-                    <span>讓整年度，</span>
-                    <span>都充滿迷人香氣。</span>
-                    <span>TINGS Candle 香氛•蠟燭•台中手作教室。</span>
-                    <span class="scroll">SCROLL</span>
+                    <span>開啟 <span class="numbers">2022</span> 年度。</span>
+                    <span>讓整個年度，</span>
+                    <span>充滿迷人香氣。</span>
+                    <span><span class="english">TINGS Aroma</span> 香氛 • 蠟燭 • 台中手作教室。</span>
+                    <span class="scroll">Scroll</span>
                 </h3>
                 <div class="photo-full">
                     <div class="top"></div>
@@ -29,40 +29,50 @@
             <div class="background-logo"></div>
         </div>
     </section>
-    
+
     <!-- News list -->
     <section id="news-list">
         <div class="news">
             <div class="news-item">
                 <div class="date">2022.01.07</div>
-                <div class="title">
-                    <a href="{{ route('front.news-content') }}">一月體驗課程日期</a>
-                </div>
-                <div class="content">以自製香氛開啟2022年度，讓整年度都充滿迷人香氣。</div>
+                <a class="title" href="{{ route('front.news-content') }}">
+                    一月體驗課程日期
+                </a>
+                <div class="content">以自製香氛開啟 2022 年度，讓整年度都充滿迷人香氣。</div>
             </div>
             <div class="news-item">
                 <div class="date">2022.01.06</div>
-                <div class="title">一月香氛蠟燭體驗</div>
+                <a class="title" href="{{ route('front.news-content') }}">
+                    一月香氛蠟燭體驗
+                </a>
                 <div class="content">自製香氛製品，調香體驗課程，調製專屬香氣，製成蠟燭擴香，隨心所欲表現。</div>
             </div>
             <div class="news-item">
                 <div class="date">2022.01.02</div>
-                <div class="title">2022的第一堂課程</div>
+                <a class="title" href="{{ route('front.news-content') }}">
+                    2022 的第一堂課程
+                </a>
                 <div class="content">以調香開始了嶄新的一年，調製屬於自己的香氣，讓瞬間成為永恆。</div>
             </div>
             <div class="news-item">
                 <div class="date">2021.12.31</div>
-                <div class="title">2021年的最後一天</div>
-                <div class="content">還是跟學員們渡過，結束最後一堂課。感謝今年所有參與TINGS Candle課程的同學們，祝大家新年快樂！</div>
+                <a class="title" href="{{ route('front.news-content') }}">
+                    2021 年的最後一天
+                </a>
+                <div class="content">跟學員們渡過，結束最後一堂課。感謝今年所有參與 TINGS Candle 課程的同學們，祝大家新年快樂！</div>
             </div>
             <div class="news-item">
                 <div class="date">2021.12.29</div>
-                <div class="title">調香體驗不只能做香水</div>
+                <a class="title" href="{{ route('front.news-content') }}">
+                    調香體驗不只能做香水
+                </a>
                 <div class="content">也能製作擴香瓶或蠟燭，今年最後一天調製香氛，讓自己在新年煥然一新。</div>
             </div>
             <div class="news-item">
                 <div class="date">2021.12.28</div>
-                <div class="title">再過幾天就要迎接跨年</div>
+                <a class="title" href="{{ route('front.news-content') }}">
+                    再過幾天就要迎接跨年
+                </a>
                 <div class="content">在今年的最後一天一起，製作香氛蠟燭迎接新年。課程日期：12/31（五）上午10:00。</div>
             </div>
         </div>
@@ -75,5 +85,5 @@
 @endsection
 
 @section('js')
-    
+
 @endsection
